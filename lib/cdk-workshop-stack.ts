306025def0334c4e3d041ed61eef54fcf68d3ecc
sahil-jamwal <sahil.jamwal78625@gmail.com>
2022-11-27 23:11:55 +0530

    update : master | init-commit

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,32 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from "./hitCounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // AWS Lambda Resource here
+    const hello = new lambda.Function(this, `HelloHanlder`, {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: `hello.handler`, // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, "EndPoint", {
+      handler: helloWithCounter.hanlder,
+    });
+
+    new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table,
+    });
   }
 }
---
+++ lib/hitcounter.ts
@@ -0,0 +1,41 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export interface HitCounterProps {
+  /** the function for which we want to count url hits **/
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+  /** allows accessing the counter function */
+  public readonly hanlder: lambda.Function;
+
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, "Hits", {
+      partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
+    });
+    this.table = table;
+
+    this.hanlder = new lambda.Function(this, "HitCounterHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hitcounter.handler",
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: this.table.tableName,
+      },
+    });
+
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(this.hanlder);
+
+    // grant the lambda role invoke permissions to the downstream function
+    props.downstream.grantInvoke(this.hanlder);
+  }
+}
